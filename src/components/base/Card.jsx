f6cac1427329a063ed6e4a484c290958d206c918
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-29 01:07:45 +0900

    feat: Card에 내부 컴포넌트 추가

--- src/components/base/Card.jsx
+++ src/components/base/Card.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View, Text, Image } from 'react-native';
 import styled from 'styled-components';
 import size from '../../utils/size';
 
-const Card = ({ width, height, bg, onPress, children }) => {
+const Card = ({ width = 80, height = 80, bgColor, onPress }) => {
+  const topAndBottomHeight = size.height * 2;
+  const middleHeight = size.height * height - topAndBottomHeight;
+
   return (
-    <CardWrapper width={width} height={height} bg={bg} onPress={onPress}>
-      {children}
+    <CardWrapper width={width} height={height} bgColor={bgColor} onPress={onPress}>
+      <ImageBox source={{ uri: undefined }} />
+      <SectionWrapper>
+        <Top>
+          <Text>top</Text>
+        </Top>
+        <Middle height={middleHeight}>
+          <Text>middle</Text>
+        </Middle>
+        <Bottom>
+          <Text>bottom</Text>
+        </Bottom>
+      </SectionWrapper>
     </CardWrapper>
   );
 };
@@ -16,5 +30,35 @@ export default Card;
 const CardWrapper = styled(TouchableOpacity)(props => ({
   width: size.width * props.width,
   height: size.height * props.height,
-  backgroundColor: props.bg,
+  backgroundColor: props.bgColor,
 }));
+
+const ImageBox = styled(Image)({
+  width: '100%',
+  height: '100%',
+});
+
+const SectionWrapper = styled(View)({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+});
+
+const Top = styled(View)({
+  width: '100%',
+  position: 'absolute',
+  top: 0,
+});
+
+const Middle = styled(View)(props => ({
+  width: '100%',
+  position: 'absolute',
+  top: size.height * 14.8 + 'px',
+  height: props.height - 15,
+}));
+
+const Bottom = styled(View)({
+  width: '100%',
+  position: 'absolute',
+  bottom: size.height * 0 + 'px',
+});
