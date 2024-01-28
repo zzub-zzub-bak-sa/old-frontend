5402c4b7d8b08f0efc76c455674ab0897c300654
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-28 12:00:52 +0900

    feat: Input UI 구현

---
+++ src/components/base/InputField.jsx
@@ -0,0 +1,39 @@
+import { PropTypes } from 'prop-types';
+import React from 'react';
+import { TextInput } from 'react-native';
+import styled from 'styled-components';
+
+const InputField = ({ placeholder, value, onChangeValue, color, placeholderColor, bg }) => {
+  return (
+    <Input
+      placeholder={placeholder}
+      placeholderTextColor={placeholderColor}
+      value={value}
+      onChangeText={text => onChangeValue(text)}
+      color={color}
+      bg={bg}
+    />
+  );
+};
+
+InputField.defaultProps = {
+  color: '#242424',
+  placeholderColor: '#242424',
+  bg: 'tomato',
+};
+
+InputField.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  placeholderColor: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.object]).isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  bg: PropTypes.string,
+};
+
+export default InputField;
+
+const Input = styled(TextInput)`
+  color: ${({ color }) => color};
+  background-color: ${({ bg }) => bg};
+`;
