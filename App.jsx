69bad0578c9f0fab6857c6f4b508c86de4be6a0e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-30 22:34:04 +0900

    feat: 폰트 및 컬러 추가

--- App.jsx
+++ App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/navigations/StackNavigation';
 GestureHandlerRootView;
+import * as Font from 'expo-font';
 
 function App() {
+  useEffect(() => {
+    const getFont = async () => {
+      await Font.loadAsync({
+        semibold: require('./src/assets/fonts/SUIT-SemiBold.ttf'),
+        medium: require('./src/assets/fonts/SUIT-Medium.ttf'),
+        bold: require('./src/assets/fonts/SUIT-Bold.ttf'),
+        extrabold: require('./src/assets/fonts/SUIT-ExtraBold.ttf'),
+      });
+    };
+
+    getFont();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
