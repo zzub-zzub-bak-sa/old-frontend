7937c91447af066c414b39bc1ae0ecc1c5d2048d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-31 11:37:37 +0900

    feat: Button UI 수정

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import styled from 'styled-components/native';
 import size from '../../utils/size';
-import font from '../../utils/font';
+import { colors } from '../../styles/colors';
+import { subtitle2 } from '../../styles/fonts';
 
-const Button = ({ width = 80, height = 24, bgColor, text, color, onPress }) => {
+const Button = ({
+  width = 350,
+  height = 54 | 36,
+  varient = 'outlined' | 'filled' | 'none',
+  color = 'primary' | 'default' | 'disable',
+  text,
+  onPress,
+}) => {
+  const [style, setStyle] = useState({
+    borderColor: '',
+    bgColor: '',
+    textColor: '',
+  });
+
+  useEffect(() => {
+    switch (color) {
+      case 'primary':
+        if (varient === 'outlined') {
+          return setStyle({ borderColor: colors.orange, bgColor: '', textColor: colors.orange });
+        } else if (varient === 'filled') {
+          return setStyle({
+            borderColor: colors.orange,
+            bgColor: colors.orange,
+            textColor: '#fff',
+          });
+        } else if (varient === 'none') {
+          return setStyle({
+            borderColor: '',
+            bgColor: '',
+            textColor: colors.orange,
+          });
+        }
+      case 'disable':
+        if (varient === 'outlined') {
+          return setStyle({ borderColor: colors.grey[100], bgColor: '', textColor: '#fff' });
+        } else if (varient === 'filled') {
+          return setStyle({
+            borderColor: color.grey[100],
+            bgColor: color.grey[100],
+            textColor: '#fff',
+          });
+        } else if (varient === 'none') {
+          return setStyle({
+            borderColor: '',
+            bgColor: '',
+            textColor: '#fff',
+          });
+        }
+      case 'default':
+        if (varied === 'outlined') {
+          return setStyle({ borderColor: '#fff', bgColor: '', textColor: '#fff' });
+        } else if (varied === 'filled') {
+          return setStyle({ borderColor: '#fff', bgColor: '#fff', textColor: colors.black });
+        } else if (varient === 'none') {
+          return setStyle({
+            borderColor: '',
+            bgColor: '',
+            textColor: '#fff',
+          });
+        }
+    }
+  }, [varient, color]);
+
   return (
-    <ButtonWrapper width={width} height={height} bg={bg} onPress={onPress}>
-      <ButtonText color={color}>{text}</ButtonText>
+    <ButtonWrapper width={width} height={height | 54} onPress={onPress} buttonStyle={style}>
+      <ButtonText height={height} buttonStyle={style}>
+        {text}
+      </ButtonText>
     </ButtonWrapper>
   );
 };
@@ -15,15 +80,18 @@ const Button = ({ width = 80, height = 24, bgColor, text, color, onPress }) => {
 export default Button;
 
 const ButtonWrapper = styled(TouchableOpacity)`
-  width: ${({ width }) => size.width * width}%;
-  height: ${({ height }) => size.height * height}%;
-  background-color: ${({ bgColor }) => bgColor};
-  justify-content: center;
+  width: ${({ width }) => size.width * width}px;
+  height: ${({ height }) => size.height * height}px;
+  background-color: ${({ buttonStyle }) => buttonStyle.bgColor};
+  border: 1px solid ${({ buttonStyle }) => buttonStyle.borderColor};
+  border-radius: 12px;
   align-items: center;
+  padding-top: 16px;
+  padding-bottom: 16px;
 `;
 
 const ButtonText = styled(Text)(props => ({
-  color: props.color,
-  fontSize: font(12),
-  fontWeight: 500,
+  fontFamily: subtitle2.semibold.fontFamily,
+  fontSize: subtitle2.semibold.fontSize,
+  color: props.buttonStyle.textColor,
 }));
