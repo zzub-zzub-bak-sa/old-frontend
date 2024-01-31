dc22cbcfb494ee0155978a19f3006ffd957a61ce
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-31 16:01:16 +0900

    feat: Card UI 수정

--- src/components/base/Card.jsx
+++ src/components/base/Card.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import { TouchableOpacity, View, Text, Image } from 'react-native';
 import styled from 'styled-components';
 import size from '../../utils/size';
+import { body1, body3 } from '../../styles/fonts';
+import Check from '../../assets/icons/Check';
+import { colors } from '../../styles/colors';
 
-const Card = ({ width = 80, height = 80, bgColor, onPress, renderTop, renderBottom }) => {
+const Card = ({ title = '', numberOfLinks = 0, onPress, isSelected }) => {
+  const handleSliceText = () => {
+    return title.length > 14 ? title.slice(0, 14) + '...' : title.length;
+  };
+
   return (
-    <CardWrapper width={width} height={height} bgColor={bgColor} onPress={onPress}>
-      <ImageBox source={{ uri: undefined }} />
-      <SectionWrapper>
-        <Top>{renderTop()}</Top>
-        <Bottom>{renderBottom()}</Bottom>
-      </SectionWrapper>
+    <CardWrapper onPress={onPress}>
+      <ImageBox source={require('../../assets/images/logo.png')} />
+      <TextWrapper>
+        <Title>{handleSliceText()}</Title>
+        <Subtitle>{numberOfLinks}개의 링크</Subtitle>
+      </TextWrapper>
+      {isSelected && (
+        <SelectBox>
+          <Check size={24} color={colors.orange} />
+        </SelectBox>
+      )}
     </CardWrapper>
   );
 };
@@ -18,30 +30,37 @@ const Card = ({ width = 80, height = 80, bgColor, onPress, renderTop, renderBott
 export default Card;
 
 const CardWrapper = styled(TouchableOpacity)`
-  width: ${({ width }) => size.width * width}%;
-  height: ${({ height }) => size.height * height}%;
-  background-color: ${({ bgColor }) => bgColor};
+  width: '100%';
+  height: ${size.height * 96};
+  flex-direction: row;
+  align-items: center;
+  background-color: black;
+  padding: 0 ${size.width * 20}px;
 `;
 
 const ImageBox = styled(Image)`
-  width: '100%';
-  height: '100%';
+  width: ${size.width * 64}px;
+  height: ${size.height * 64}px;
+  margin-right: ${size.width * 16}px;
 `;
 
-const SectionWrapper = styled(View)`
-  position: 'absolute';
-  width: '100%';
-  height: '100%';
+const TextWrapper = styled(View)`
+  gap: ${size.height * 12}px;
 `;
 
-const Top = styled(View)`
-  width: '100%';
-  position: 'absolute';
-  top: 0;
+const Title = styled(Text)`
+  font-family: ${body1.medium.fontFamily};
+  font-size: ${body1.medium.fontSize};
+  color: white;
 `;
 
-const Bottom = styled(View)`
-  width: '100%';
-  position: 'absolute';
-  bottom: 0 + 'px';
+const Subtitle = styled(Text)`
+  font-family: ${body3.medium.fontFamily};
+  font-size: ${body3.medium.fontSize};
+  color: white;
+`;
+
+const SelectBox = styled(View)`
+  position: absolute;
+  right: ${size.width * 20}px;
 `;
